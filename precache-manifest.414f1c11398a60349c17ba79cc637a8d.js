e9a251e9dfd86ae700cb2018104187b57ba8f6f5
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-07-21 16:04:02 +0200

    Updates

---
+++ precache-manifest.414f1c11398a60349c17ba79cc637a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73feca520f393cd4b19ea658ffd58a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0bfa0b3f358c1b2f2a0",
+    "url": "/static/css/main.eae631ad.chunk.css"
+  },
+  {
+    "revision": "dfc319b47d47750c507f",
+    "url": "/static/js/2.58774806.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58774806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0bfa0b3f358c1b2f2a0",
+    "url": "/static/js/main.30bdb782.chunk.js"
+  },
+  {
+    "revision": "6c725d5ce54ceabe8e25",
+    "url": "/static/js/runtime-main.55c2e157.js"
+  }
+]);
